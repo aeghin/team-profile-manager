76c778fb4f3772af91936770cb6723a5d4309f01
Edward Castaneda <aeghin7@gmail.com>
2021-10-25 19:51:43 -0500

    mostly done, just missing css, readme.

---
+++ index.js
@@ -0,0 +1,142 @@
+
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const fs = require("fs");
+const inquirer = require("inquirer");
+const generateTeamRoster = require("./src/generateHTML");
+let teamMembers = [];
+
+menu = () => {
+    createManager = () => {
+        inquirer.prompt([
+            {
+                type: "input",
+                name: "name",
+                message: "Enter the manager's name",
+            },
+            {
+                type: "input",
+                name: "id",
+                message: "What is the manager's id?",
+            },
+            {
+                type: "input",
+                name: "email",
+                message: "What is the manager's email?",
+            },
+            {
+                type: "input",
+                name: "office",
+                message: "What is the manager's office number?",
+            }
+        ]).then(({ name, id, email, office }) => {
+            const manager = new Manager(name, id, email, office);
+            teamMembers.push(manager);
+            nextTeamMember();
+        });
+
+    }
+    createManager();
+
+    nextTeamMember = () => {
+        inquirer.prompt([
+            {
+                type: "list",
+                name: "member",
+                message: "Which type of team memeber would you like to add?",
+                choices: ['Engineer', 'Intern', 'None']
+            }
+        ]).then(({ member }) => {
+            if (member === "Engineer") {
+                createEngineer();
+            } else if (member === "Intern") {
+                createIntern();
+            } else {
+                const teamContent = generateTeamRoster(teamMembers);
+                fs.writeFile("./dist/team-roster.html", teamContent, (err) =>
+                    err ? console.log(err) : console.log("Successfully created your team roster!")
+                );
+            }
+        });
+    }
+
+    createEngineer = () => {
+        inquirer.prompt([
+            {
+                type: "input",
+                name: "name",
+                message: "Enter the engineer's name",
+            },
+            {
+                type: "input",
+                name: "id",
+                message: "What is the engineer's id?",
+            },
+            {
+                type: "input",
+                name: "email",
+                message: "What is the engineer's email?",
+            },
+            {
+                type: "input",
+                name: "github",
+                message: "What is the engineer's GitHub username?",
+            }
+        ]).then(({ name, id, email, github }) => {
+            const engineer = new Engineer(name, id, email, github);
+            teamMembers.push(engineer);
+            nextTeamMember();
+        });
+    }
+
+    createIntern = () => {
+        inquirer.prompt([
+            {
+                type: "input",
+                name: "name",
+                message: "Enter the intern's name",
+            },
+            {
+                type: "input",
+                name: "id",
+                message: "What is the intern's id?",
+            },
+            {
+                type: "input",
+                name: "email",
+                message: "What is the intern's email?",
+            },
+            {
+                type: "input",
+                name: "school",
+                message: "What school does the intern attend?",
+            }
+        ]).then(({ name, id, email, school }) => {
+            const intern = new Intern(name, id, email, school);
+            teamMembers.push(intern);
+            nextTeamMember();
+        });
+    }
+}
+
+menu();
+
+// start()
+//     switch (response.role) {
+//         case "Manager":
+//             addManager()
+//             break;
+
+//         case "Engineer":
+//             addEngineer()
+//             break;
+
+//         case "Intern":
+
+//             break;
+
+//         default:
+//             break;
+//     }
+//     "make another employee?"
---
+++ src/generateHTML.js
@@ -0,0 +1,102 @@
+function renderManager(manager) {
+    return `<figure class="card">
+    <div class="card-body">
+        <h2>${manager.name}</h2>
+        <h3><i class="fa fa-coffee"></i> ${manager.getRole()}</h3>
+    </div>
+    <ul class="list-group list-group-flush">
+        <li class="list-group-item">ID: ${manager.id}</li>
+        <li class="list-group-item">Email: <a href="mailto:${manager.email}">${manager.email}</a></li>
+        <li class="list-group-item">Office Number: ${manager.officeNumber}</li>
+    </ul>
+</figure>`;
+}
+
+
+function renderEngineer(engineer) {
+    return `<figure class="card">
+        <div class="card-body">
+            <h2>${engineer.name}</h2>
+            <h3><i class="fa fa-cogs"></i> ${engineer.getRole()}</h3>
+        </div>
+        <ul class="list-group list-group-flush">
+            <li class="list-group-item">ID: ${engineer.id}</li>
+            <li class="list-group-item">Email: <a href="mailto:${engineer.email}">${engineer.email}</a></li>
+            <li class="list-group-item">GitHub: <a href="https://github.com/${engineer.github}" target="_blank">${engineer.github}</a></li>
+        </ul>
+        </figure>`;
+}
+
+
+function renderIntern(intern) {
+    return `<figure class="card">
+        <div class="card-body">
+            <h2>${intern.name}</h2>
+            <h3><i class="fa fa-graduation-cap"></i> ${intern.getRole()}</h3>
+        </div>
+        <ul class="list-group list-group-flush">
+            <li class="list-group-item">ID: ${intern.id}</li>
+            <li class="list-group-item">Email: <a href="mailto:${intern.email}">${intern.email}</a></li>
+            <li class="list-group-item">School: ${intern.school}</li>
+        </ul>
+        </figure>`;
+}
+
+function generateTeamRoster(teamMembers) {
+    const employeeArray = [];
+
+    const managerArray = teamMembers.filter((member) => {
+        return member.getRole() === "Manager"
+    });
+
+    const engineerArray = teamMembers.filter((member) => {
+        return member.getRole() === "Engineer"
+    });
+
+    const internArray = teamMembers.filter((member) => {
+        return member.getRole() === "Intern"
+    });
+
+    managerArray.forEach(manager => {
+        const renderedManager = renderManager(manager);
+        employeeArray.push(renderedManager);
+    });
+
+    engineerArray.forEach(engineer => {
+        const renderedEngineer = renderEngineer(engineer);
+        employeeArray.push(renderedEngineer);
+    });
+
+    internArray.forEach(intern => {
+        const renderedIntern = renderIntern(intern);
+        employeeArray.push(renderedIntern);
+    });
+    return `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link rel="stylesheet" href="styles.css">
+        <title>Team Roster</title>
+    </head>
+    <body>
+    
+        <header class="jumbotron">
+            <h1>My Team</h1>
+        </header>
+        <main>
+            ${employeeArray.join("")}
+        </main>
+    
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/js/bootstrap.min.js"></script>
+        <script src="../index.js"></script>
+    </body>
+    </html>`;
+}
+
+
+module.exports = generateTeamRoster;
